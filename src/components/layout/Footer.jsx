4d6bcd4ba716b4ba318e9ef12494fa61dbf8f9ca
NIcolasRiosPulidoUnoSquare <nicolas.rios@unosquare.com>
2021-08-03 16:17:02 -0500

    ajustes footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,15 @@
+import {
+    FacebookShareButton,
+    WhatsappShareButton,
+    LinkedinShareButton,
+    EmailShareButton,
+    FacebookShareCount,
+    FacebookIcon,
+    WhatsappIcon,
+    LinkedinIcon,
+    EmailIcon
+} from 'react-share';
+import { DocumentsOutline } from 'react-ionicons'
 import styles from '../../styles/Layout.module.css';
 
 import LinksApps from '../general/LinksApps';
@@ -10,8 +22,65 @@ const Footer = () => {
             <div className={styles.FooterCard}>
                 <div className={styles.FooterLine} />
                 <div className={styles.FooterTexContainer} >
-                    <p className={styles.FooterCardTitle}>Donec viverra purus a blandit</p>
-                    <p className={styles.FooterCardDesc}>Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor.</p>
+                    <p className={styles.FooterCardTitle}>
+                        Compartir en:
+                    </p>
+                    <div className={styles.socialCOntainer}>
+                        <div>
+                            <FacebookShareButton url={"https://peing.net/ja/"}>
+                                <FacebookIcon
+                                    size={40}
+                                    round
+                                />
+                            </FacebookShareButton>
+                            <FacebookShareCount url={"https://peing.net/ja/"}>
+                                {shareCount => <span className="myShareCountWrapper">{shareCount}</span>}
+                            </FacebookShareCount>
+                        </div>
+                        <div>
+                            <WhatsappShareButton url={"https://peing.net/ja/"}>
+                                <WhatsappIcon
+                                    size={40}
+                                    round
+                                />
+                            </WhatsappShareButton>
+                        </div>
+                        <div>
+                            <LinkedinShareButton url={"https://peing.net/ja/"}>
+                                <LinkedinIcon
+                                    size={40}
+                                    round
+                                />
+                            </LinkedinShareButton>
+                        </div>
+                        <div>
+                            <EmailShareButton url={"https://peing.net/ja/"}>
+                                <EmailIcon
+                                    size={40}
+                                    round
+                                />
+                            </EmailShareButton>
+                        </div>
+                    </div>
+                    <p className={styles.FooterCardshare}>
+                        O copia el link y compartelo:
+                    </p>
+                    <div className="p_r">
+                        <input
+                            defaultValue="https://otto-app.com/"
+                            desable="true"
+                            className={styles.inputShare}
+                        />
+                        <DocumentsOutline
+                            color={'#5A38FD'}
+                            height="25px"
+                            width="25px"
+                            className={styles.iconInputShare}
+                        />
+                    </div>
+                    <p className={styles.FooterCardDesc}>
+                        ¡Descargala ya!
+                    </p>
                     <LinksApps />
                 </div>
             </div>
