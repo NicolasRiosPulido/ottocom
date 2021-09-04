d527b026b40330db1e934e0dbf145825dc244c88
NIcolasRiosPulidoUnoSquare <nicolas.rios@unosquare.com>
2021-09-04 10:16:42 -0500

    correccion error

--- pages/tyc.jsx
+++ pages/tyc.jsx
@@ -1,10 +1,10 @@
-import { Document } from "react-pdf";
 import styles from '../src/styles/tyc.module.css';
 
 const TYC = "https://doc-archivos.s3.us-west-2.amazonaws.com/tyc_otto.pdf"
 
 const tyc = () => {
     return (
+
         <iframe
             src={TYC}
             frameborder="0"
