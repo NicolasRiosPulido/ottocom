d7040ebe4ac2c7fa1658b4841081474cb0b00513
NIcolasRiosPulidoUnoSquare <nicolas.rios@unosquare.com>
2021-09-22 14:15:23 -0500

    test 2 url

--- src/components/general/Descargar.jsx
+++ src/components/general/Descargar.jsx
@@ -1,37 +1,29 @@
 
+import React, { useEffect, useState } from 'react';
 import * as rdd from 'react-device-detect';
-
 import styles from '../../styles/General.module.css';
 
 import { APP_STORE, GOOGLE_PLAY } from '../../lisbs/const';
 
 const Descargar = () => {
 
-    if (rdd.isAndroid || rdd.isIOS) {
-        return (
-            <a
-                className={styles.btnContainer}
-                href={GOOGLE_PLAY}
-                target="_blank"
-                rel="noreferrer"
-            >
-                <p>Descargar</p>
-            </a>
-        )
+    const [url, seturl] = useState('')
 
-    } else {
-        return (
-            <a
-                className={styles.btnContainer}
-                href={APP_STORE}
-                target="_blank"
-                rel="noreferrer"
-            >
-                <p>Descargar</p>
-            </a>
-        )
-    }
+    useEffect(() => {
+        const urltipe = rdd.isAndroid ? GOOGLE_PLAY : APP_STORE;
+        seturl(urltipe)
+    }, [])
 
+    return (
+        <a
+            className={styles.btnContainer}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+        >
+            <p>Descargar</p>
+        </a>
+    )
 }
 
 export default Descargar
