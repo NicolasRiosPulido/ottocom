f518f21a468b6843fd0e5304e5f476cfbfa09ec7
Juan Vargas <jvargasp@semana.com>
2021-10-03 12:41:54 -0500

    Update tyc.jsx

--- pages/tyc.jsx
+++ pages/tyc.jsx
@@ -1,6 +1,6 @@
 import styles from '../src/styles/tyc.module.css';
 
-const TYC = "https://doc-archivos.s3.us-west-2.amazonaws.com/tyc_otto.pdf"
+const TYC = "https://avatars.otto-app.com/doc/tyc-ottoapp-20210811.pdf"
 
 const tyc = () => {
     return (
